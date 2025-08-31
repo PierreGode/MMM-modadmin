23a70ff613496d8db0c335984883f1b0c25b3315
PierreGode <pierre@gode.one>
2025-08-31 22:18:28 +0200

    feat: add module admin web portal

--- MMM-modadmin.js
+++ MMM-modadmin.js
@@ -1,1 +1,14 @@
+Module.register("MMM-ModAdmin", {
+  defaults: {
+    adminPort: 8081
+  },
 
+  start() {
+    this.sendSocketNotification("INIT", this.config);
+  },
+
+  getDom() {
+    // This module intentionally renders nothing on the mirror
+    return document.createElement("div");
+  }
+});
